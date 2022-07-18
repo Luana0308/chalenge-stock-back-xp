0f0f102253c771a1ba596a73dceced0d145860e5
Luana <luanaff-2008@hotmail.com>
2022-07-17 22:10:46 -0300

    test: add model Client sequelize

---
+++ src/tests/database/models/client.test.js
@@ -0,0 +1,15 @@
+const {
+  sequelize, dataTypes,
+  checkModelName, checkPropertyExists
+} = require('sequelize-test-helpers')
+
+const ClientModel = require('../../../database/models/client')
+
+describe('src/models/Simple', () => {
+  const Model = ClientModel(sequelize, dataTypes)
+  const instance = new Model()
+  const properties = ['id', 'name', 'email', 'password', 'value']
+
+  checkModelName(Model)('Client')
+  properties.forEach(checkPropertyExists(instance))
+})
