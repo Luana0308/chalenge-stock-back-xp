11139a89f5112e2ec73e502a77b1b3fcf23b40ac
Luana <luanaff-2008@hotmail.com>
2022-07-18 21:00:43 -0300

    build: instal cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const routers = require('./routes/router')
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 app.use(routers)
+app.use(cors())
 
 module.exports = app
--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     dialect: 'mysql'
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
+    username: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || '080393',
+    database: process.env.MYSQL_DB_NAME || 'stock-test-back-xp',
+    host: process.env.MYSQL_HOST || 'localhost',
     dialect: 'mysql'
   }
 }
